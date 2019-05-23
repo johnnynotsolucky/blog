f54456637b59b759b1f26541d6f8e263ecf54b2e
Tyrone Tudehope <tyronetudehope@gmail.com>
2019-05-23 21:26:13 +0200

    fix build issue with StaticQuery

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,121 +1,96 @@
 import React from "react"
 import { Link, StaticQuery, graphql } from "gatsby"
-import Image from "gatsby-image"
+import Avatar from './Avatar'
 
 import { rhythm, scale } from "../utils/typography"
-
-const Avatar = ({ width = 30}) =>
-  <StaticQuery
-    query={bioQuery}
-    render={data => {
-      const { author } = data.site.siteMetadata
-      return (
-        <div>
-          <Image
-            fixed={data.avatar.childImageSharp.fixed}
-            alt={author}
-            style={{
-              marginRight: rhythm(1 / 2),
-              marginBottom: 0,
-              borderRadius: `100%`,
-              width,
-              height: width,
-              minWidth: width,
-            }}
-            imgStyle={{
-              borderRadius: `50%`,
-            }}
-          />
-        </div>
-      )
-    }}
-  />
 
 class Layout extends React.Component {
   render() {
     const { location, title, children } = this.props
     const rootPath = `${__PATH_PREFIX__}/`
-    let header
 
-    if (location.pathname === rootPath) {
-      header = (
-        <h1
-          style={{
-            ...scale(1),
-            display: 'flex',
-            fontFamily: `Montserrat, sans-serif`,
-            marginBottom: rhythm(1),
-            marginTop: 0,
-          }}
-        >
-          <Avatar width={50} />
-          <Link
-            style={{
-              boxShadow: `none`,
-              textDecoration: `none`,
-              color: `inherit`,
-            }}
-            to={`/`}
-          >
-            {title}
-          </Link>
-        </h1>
-      )
-    } else {
-      header = (
-        <h3
-          style={{
-            display: 'flex',
-            fontFamily: `Montserrat, sans-serif`,
-            lineHeight: '1.25em',
-            marginTop: 0,
-          }}
-        >
-          <Avatar />
-          <Link
-            style={{
-              boxShadow: `none`,
-              textDecoration: `none`,
-              color: `inherit`,
-            }}
-            to={`/`}
-          >
-            {title}
-          </Link>
-        </h3>
-      )
-    }
     return (
-      <div
-        style={{
-          marginLeft: `auto`,
-          marginRight: `auto`,
-          maxWidth: rhythm(40),
-          padding: `${rhythm(1.5)} ${rhythm(3 / 4)}`,
-        }}
-      >
-        <header>{header}</header>
-        <main>{children}</main>
-        <StaticQuery
-          query={bioQuery}
-          render={data => {
-            const { social } = data.site.siteMetadata
-            return (
+      <StaticQuery
+        query={query}
+        render={data => {
+          let header
+          if (location.pathname === rootPath) {
+            header = (
+              <h1
+                style={{
+                  ...scale(1),
+                  display: 'flex',
+                  fontFamily: `Montserrat, sans-serif`,
+                  marginBottom: rhythm(1),
+                  marginTop: 0,
+                }}
+              >
+                <Avatar data={data} width={50} />
+                <Link
+                  style={{
+                    boxShadow: `none`,
+                    textDecoration: `none`,
+                    color: `inherit`,
+                  }}
+                  to={`/`}
+                >
+                  {title}
+                </Link>
+              </h1>
+            )
+          } else {
+            header = (
+              <h3
+                style={{
+                  display: 'flex',
+                  fontFamily: `Montserrat, sans-serif`,
+                  lineHeight: '1.25em',
+                  marginTop: 0,
+                }}
+              >
+                <Avatar data={data} />
+                <Link
+                  style={{
+                    boxShadow: `none`,
+                    textDecoration: `none`,
+                    color: `inherit`,
+                  }}
+                  to={`/`}
+                >
+                  {title}
+                </Link>
+              </h3>
+            )
+          }
+
+          const { social } = data.site.siteMetadata
+
+          return (
+            <div
+              style={{
+                marginLeft: `auto`,
+                marginRight: `auto`,
+                maxWidth: rhythm(40),
+                padding: `${rhythm(1.5)} ${rhythm(3 / 4)}`,
+              }}
+            >
+              <header>{header}</header>
+              <main>{children}</main>
               <footer>
                 © {new Date().getFullYear()} <a href={`https://twitter.com/${social.twitter}`}>{social.twitter}</a>. Built with
                 {` `}
                 <a href="https://www.gatsbyjs.org">Gatsby</a>
               </footer>
-            )
-          }}
-        />
-      </div>
+            </div>
+          )
+        }}
+      />
     )
   }
 }
 
-const bioQuery = graphql`
-  query AvatarQuery {
+const query = graphql`
+  query LayoutQuery {
     avatar: file(absolutePath: { regex: "/avatar.jpg/" }) {
       childImageSharp {
         fixed(width: 50, height: 50) {
